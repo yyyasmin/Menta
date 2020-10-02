92712c46034897687bffa62684f663221b90bdee
Yasmin <Yasminamran@gmail.com>
2020-10-02 17:31:31 +0300

    add timeout to procfile

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ from app.models import User, Student, Teacher, Profile, Strength, Weakness, Role
 
 app = create_app()
            
-if __name__ == '__main__':
+if __name__ == "__main__":
 	print("Running main with debug mode")
 	#FROM https://stackoverflow.com/questions/31264826/start-a-flask-application-in-separate-thread
 	#FROM https://stackoverflow.com/questions/38822829/sqlalchemy-raises-queuepool-limit-of-size-10-overflow-10-reached-connection-tim/47151701#47151701
-	app.run(debug=True, threaded=False)
+	app.run(debug=True)
             
 #if running on heroku these 3 lines shouldnt be running- delete them
