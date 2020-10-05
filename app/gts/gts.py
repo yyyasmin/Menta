862d42a1d5a1af1c09168fd2040ada25c1c47d05
Yasmin <Yasminamran@gmail.com>
2020-10-05 09:09:13 +0300

    gta query

--- app/gts/gts.py
+++ app/gts/gts.py
@@ -53,8 +53,8 @@ def edit_gts():
     #DEBUG ONLY
     
     
-    #gts = General_txt.query.order_by(General_txt.class_name).order_by(General_txt.title).all() 
-    gts = General_txt.query.all() 
+    gts = General_txt.query.order_by(General_txt.type).order_by(General_txt.title).all() 
+    #gts = General_txt.query.all() 
 
     '''
     print("")
